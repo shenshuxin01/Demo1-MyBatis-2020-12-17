26743aed42e45f0b2d1f340e3c115bb8f9fb6eb9
shenshuxin <ssx17852015601@163.com>
2020-12-17 14:05:48 +0800

    demo2

--- src/main/java/zkr/ssx/dao/UserMapper.java
+++ src/main/java/zkr/ssx/dao/UserMapper.java
@@ -1,19 +1,34 @@
 package zkr.ssx.dao;
 
-import org.apache.ibatis.annotations.CacheNamespace;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.mapping.FetchType;
 import zkr.ssx.entity.User;
 
+@CacheNamespace(blocking = true)
 public interface UserMapper {
+
     @Select("select * from user where user_name = #{userName} ")
     @Results(id = "userMap",
              value = {
                 @Result(property = "userName",column = "user_name"),
                 @Result(property = "passWord",column = "pass_word")
+
              }
     )
+
+//    @Results(id = "userMap",
+//            value = {
+//
+//                    //一对多 关联表连接查询
+//                    @Result(property = "userName",column = "user_name",
+//                            many = @Many(select = "select ... ",fetchType = FetchType.LAZY)),
+//
+//                    //一对一 关联表连接查询
+//                    @Result(property = "passWord",column = "pass_word",
+//                            one = @One(select = "select ... ",fetchType = FetchType.EAGER))
+//
+//            }
+//    )
     User selectByName(User user);
 
 }
